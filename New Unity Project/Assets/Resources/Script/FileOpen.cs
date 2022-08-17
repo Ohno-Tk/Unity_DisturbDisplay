329970fee3ac345d692966e28216b4a09e5cd6ad
Ohno <51406176+Ohno-Tk@users.noreply.github.com>
2022-08-17 23:19:13 +0900

    [add]スクロールビューに読み込んだ画像名を表示

--- New Unity Project/Assets/Resources/Script/FileOpen.cs
+++ New Unity Project/Assets/Resources/Script/FileOpen.cs
@@ -10,6 +10,10 @@ public class FileOpen : MonoBehaviour
 {
     [SerializeField]
     private GameObject TextureUIObject;
+    [SerializeField]
+    private GameObject ScrollViewObject_Content;
+    [SerializeField]
+    private GameObject TextPrefabObject;
     private List<string> ImagePathList;
     private Change_ImageTexture imagetexture;
 
@@ -26,10 +30,19 @@ public class FileOpen : MonoBehaviour
         var path = EditorUtility.OpenFilePanel("Image files", "", "png,jpg");
         if (string.IsNullOrEmpty(path)){return;}
 
-        Debug.Log(path);
+        Debug.Log("ファイルパス名：" + path);
+        Debug.Log("ファイル名：" + System.IO.Path.GetFileName(path));
 
         // リストに追加
         ImagePathList.Add(path);
+
+        // スクロールビューに登録
+        GameObject Obj = (GameObject)Instantiate (TextPrefabObject, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+        Obj.transform.parent = ScrollViewObject_Content.transform;
+        Vector3 Pos = Obj.GetComponent<RectTransform>().localPosition;
+        Obj.GetComponent<RectTransform>().localPosition = new Vector3(Pos.x, Pos.y, 1.0f);
+        Obj.GetComponent<RectTransform>().localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        Obj.GetComponent<Text>().text = System.IO.Path.GetFileName(path);
     }
 
     // スプライトの作成
@@ -48,6 +61,12 @@ public class FileOpen : MonoBehaviour
     {
         ImagePathList.Clear();
         imagetexture.ListAllClear();
+
+        // スクロールビューのテキストを削除
+        foreach(Transform i in ScrollViewObject_Content.transform )
+        {
+            GameObject.Destroy(i.gameObject);
+        }
 
         Debug.Log("リストを削除しました～");
     }
